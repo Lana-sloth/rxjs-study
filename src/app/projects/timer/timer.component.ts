2b267a6de2fdc867791de34dec4a110efc548534
Lana-sloth <lana.sloth@gmail.com>
2018-09-06 13:12:49 +0300

    added timer that starts to tick immediately, menu links are in list now

--- src/app/projects/timer/timer.component.ts
+++ src/app/projects/timer/timer.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval, timer, Subject, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-timer',
   styleUrls: ['./timer.component.css'],
   template: `
-  <h1>Timer!</h1>
+  <h1>Timer</h1>
+  <div>{{ clock | async }}</div>
+  <button (click)="reset$.next()">reset</button>
   `
 })
 export class TimerComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  reset$ = new Subject()
+  clock = interval(1000);
 }
