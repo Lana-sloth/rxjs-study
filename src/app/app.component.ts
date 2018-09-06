31d7939a42c32e25c6fca51e7b53542b210354cb
Lana-sloth <lana.sloth@gmail.com>
2018-09-06 11:31:58 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  <h1>Hello!</h1>
+  `
 })
 export class AppComponent {
   title = 'app';
